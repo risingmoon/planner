56e8d7b513b0eb68a3d1d5963d4879860e4d5bc0
Justin Lee <j.lee@pokemon.com>
2018-07-15 02:53:33 -0700

    Add WhiteNoise library

--- planner/config/settings.py
+++ planner/config/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'rest_framework',
     'todos'
 ]

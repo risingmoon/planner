c2a8603c8f441525f20a91973fe56cdd907e8fef
Justin Lee <j.lee@pokemon.com>
2018-07-15 02:48:06 -0700

    Add Permissions

--- planner/todos/views.py
+++ planner/todos/views.py
@@ -1,10 +1,12 @@
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 
 from .models import Todo
 from .serializers import TodoSerializer
 
 
 class TodoListView(generics.ListCreateAPIView):
+    permission_classes = (IsAuthenticated,)
     serializer_class = TodoSerializer
 
     def get_queryset(self):
@@ -15,6 +17,7 @@ class TodoListView(generics.ListCreateAPIView):
 
 
 class TodoView(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAuthenticated,)
     serializer_class = TodoSerializer
 
     def get_queryset(self):
